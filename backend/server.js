2010e5db0cf7ec3e32dd2db030f0ed671fa875f8
jaydeep <jaydeepsathwara272@gmail.com>
2023-06-29 11:21:59 +0530

    Setting Up Server

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,29 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
 
+// Handling Uncaught Error
+process.on('uncaughtException', (err)=>{
+    console.log("Error: ", err.message);
+    console.log("Shutting down the server due to Uncaught sException");
+        process.exit(1);
+})
+
 dotenv.config({path: "backend/config/.env"});
 
 connectDatabase();
 
-app.get('/api', (req, res) => {
-    res.json({ "userse": "asd" });
-})
+// app.get('/api', (req, res) => {
+//     res.json({ "userse": "asd" });
+// })
 
 const server = app.listen(8000, () => {
-    console.log(`Server is listing on http://localhost:${process.env.PORT}`);
+    console.log(`Server is listing on http://localhost:8000 ${process.env.PORT}`);
 })
+
+process.on('unhandledRejection',(err) => {
+    console.log("Error: ", err.message);
+    console.log("Shutting down the server due to Unhandled Promise Rejection");
+    server.close(() => {
+        process.exit(1);
+    })
+});
